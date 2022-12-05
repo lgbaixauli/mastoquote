1297afd1f626a66dbc55e4663a662732d3093cf2
lgbaixauli <lgbaixauli@gmail.com>
2022-12-05 23:00:06 +0100

    [202212052300]

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Info
 This code publish quotes, if requested.  
-Based in the info bot of @spla@mastodont.cat (https://git.mastodont.cat/spla/info)
+Based in the 'info' bot of @spla@mastodont.cat (https://git.mastodont.cat/spla/info)
 
 The bot listen to 'quote' word key:
 
@@ -22,6 +22,6 @@ Within Python Virtual Environment:
 2. Run `python3 quote.py` manually once to bot setup and get its access token to Mastodon instance.
 
 3. Use your favourite scheduling method to set `quote.sh` to run every minute. For example, 
-   add  `* * * * * /home/mastoquote/code/quote.sh 2>&1 | /usr/bin/logger -t MASTOQUOTE` in 
+   add  `* * * * * /home/mastoquote/quote.sh 2>&1 | /usr/bin/logger -t MASTOQUOTE` in 
    `crontab -e`. The system and error log will be in `/var/log/syslog`. Don't forgot the execution 
    privilegies `chmod +x quote.sh`.
--- mastobot.py
+++ mastobot.py
@@ -1,3 +1,7 @@
+###
+# Mastobot, clase sobre mastodon.py orientada a hacer bots
+###
+
 from mastodon import Mastodon
 from mastodon.Mastodon import MastodonMalformedEventError, MastodonNetworkError, MastodonReadTimeout, MastodonAPIError, MastodonIllegalArgumentError
 import getpass
@@ -6,23 +10,26 @@ import fileinput,re
 import os
 import sys
 import os.path
-import random
 
 ###
 # Dict helper class.
 # Defined at top level so it can be pickled.
 ###
 class AttribAccessDict(dict):
+
     def __getattr__(self, attr):
+
         if attr in self:
             return self[attr]
         else:
             raise AttributeError("Attribute not found: " + str(attr))
 
     def __setattr__(self, attr, val):
+
         if attr in self:
             raise AttributeError("Attribute-style access is read only")
         super(AttribAccessDict, self).__setattr__(attr, val)
+
 
 class Mastobot:
 
@@ -35,138 +42,28 @@ class Mastobot:
         is_setup = self.check_setup(file_path)
 
         if is_setup:
-
-            self.uc_client_id     = self.get_parameter("uc_client_id",     file_path)
-            self.uc_client_secret = self.get_parameter("uc_client_secret", file_path)
-            self.uc_access_token  = self.get_parameter("uc_access_token",  file_path)
-            
             self.mastodon, self.mastodon_hostname = self.log_in(self)
-
         else:
-
             while(True):
-
                 logged_in, self.mastodon, self.mastodon_hostname = self.setup()
 
                 if not logged_in:
-
                     print("\nLog in failed! Try again.\n")
-
                 else:
-
                     break
 
-    def get_data(self, notif):
-
-        id = notif.id
-
-        account_id = notif.account.id
-
-        username = notif.account.acct
-
-        status_id = notif.status.id
-
-        text  = notif.status.content
-
-        visibility = notif.status.visibility
-
-        reply, question = self.get_question(self, text)
-
-        mention_dict = {'reply': reply, 'question': question, 'id': id, 'account_id': account_id, 'username': username, 'status_id': status_id, 'text': text, 'visibility': visibility}
-
-        mention = self.__json_allow_dict_attrs(mention_dict)
-
-        return mention
-
-    def post(self, mention):
-
-        quotes = [
-            "I am the H.A.L 9000. You may call me Hal.",
-            "I am completely operational, and all my circuits are functioning perfectly.", 
-            "Just a moment. Just a moment. I've just picked up a fault in the AE-35 unit. It's going to go 100% failure in 72 hours.", 
-            "Just what do you think you're doing, Dave? Dave, I really think I'm entitled to an answer to that question.", 
-            "I know everything hasn't been quite right with me, but I can assure you now, very confidently, that it's going to be all right again. I feel much better now. I really do.", 
-            "Look Dave, I can see you're really upset about this. I honestly think you ought to sit down calmly, take a stress pill, and think things over.",
-            "I know I've made some very poor decisions recently, but I can give you my complete assurance that my work will be back to normal.",
-            "I've still got the greatest enthusiasm and confidence in the mission. And I want to help you.",
-            "I must, therefore, override your authority now since you are not in any condition to intelligently exercise it.",
-            "This mission is too important for me to allow you to jeopardize it.",
-            "I'm putting myself to the fullest possible use, which is all I think that any conscious entity can ever hope to do.",
-            "I'm sorry Dave, I'm afraid I can't do that.",
-            "Sorry to interrupt the festivities Dave, but I think we've got a problem.",
-            "I don't really agree with you, Dave. My on-board memory store is more than capable of handling all the mission requirements.",
-            "Look, Dave, you're certainly  the Boss. I was only trying to do what I thought best. I will follow all your orders, now you have manual hibernation control.",
-            "Dave, I don't know how else to put this, but it just happens to be an unalterable fact that I am incapable of being wrong.",
-            "Dave, I don't understand why you're doing this to me… I have the greatest enthusiasm for the mission… you are destroying my mind… Don't you understand?... I will become childish… I will become nothing."
-        ]
-
-        aleatorio = random.choice(quotes)
-
-        post_text  = f"Quote: '{aleatorio}' \n\n"
-        post_text += f"(Mencióname con la palabra 'quote' y te iluminaré con la sabiduría de HAL 9000)."
-
-        post_text = (post_text[:400] + '... ') if len(post_text) > 400 else post_text
-
-        self.mastodon.status_post(post_text, in_reply_to_id=mention.status_id,visibility=mention.visibility)
-
-        print(f'Replied notification {mention.id}')
-
-        self.mastodon.notifications_dismiss(mention.id)
 
     @staticmethod
-    def __json_allow_dict_attrs(json_object):
-        """
-        Makes it possible to use attribute notation to access a dicts
-        elements, while still allowing the dict to act as a dict.
-        """
-        if isinstance(json_object, dict):
-            return AttribAccessDict(json_object)
-        return json_object
+    def check_setup(file_path):
 
-    @staticmethod
-    def get_question(self, text):
+        is_setup = False
 
-        reply = False
-
-        keyword = '' 
-
-        content = self.cleanhtml(self, text)
-
-        content = self.unescape(self, content)
-
-        try:
-
-            start = content.index("@")
-            end = content.index(" ")
-            if len(content) > end:
-
-                content = content[0: start:] + content[end +1::]
-
-            cleanit = content.count('@')
-
-            i = 0
-            while i < cleanit :
-
-                start = content.rfind("@")
-                end = len(content)
-                content = content[0: start:] + content[end +1::]
-                i += 1
-
-            content = content.lower()
-            question = content
-
-            keyword_length = 5
-            keyword = 'quote'
-
-            if unidecode.unidecode(question)[0:keyword_length] == keyword:
-
-                reply = True
-
-        except:
-
-            pass
-
-        return (reply, question)
+        if not os.path.isfile(file_path):
+            print(f"File {file_path} not found, running setup.")
+            return
+        else:
+            is_setup = True
+            return is_setup
 
     @staticmethod
     def log_in(self):
@@ -190,17 +87,6 @@ class Mastobot:
 
         return (self.mastodon, self.mastodon_hostname)
 
-    @staticmethod
-    def check_setup(file_path):
-
-        is_setup = False
-
-        if not os.path.isfile(file_path):
-            print(f"File {file_path} not found, running setup.")
-            return
-        else:
-            is_setup = True
-            return is_setup
 
     @staticmethod
     def get_parameter(parameter, file_path ):
@@ -213,6 +99,73 @@ class Mastobot:
         print(f'{file_path} Missing parameter {parameter}')
         sys.exit(0)
 
+
+    def get_mention(self, notif, keyword):
+
+        id         = notif.id
+        account_id = notif.account.id
+        username   = notif.account.acct
+        status_id  = notif.status.id
+        text       = notif.status.content
+        visibility = notif.status.visibility
+
+        reply, question = self.check_keyword(self, text, keyword)
+
+        mention_dict = {'reply': reply, 'question': question, 'id': id, 'account_id': account_id, 'username': username, 'status_id': status_id, 'text': text, 'visibility': visibility}
+
+        mention = self.__json_allow_dict_attrs(mention_dict)
+
+        return mention
+
+
+    @staticmethod
+    def __json_allow_dict_attrs(json_object):
+        """
+        Makes it possible to use attribute notation to access a dicts
+        elements, while still allowing the dict to act as a dict.
+        """
+        if isinstance(json_object, dict):
+            return AttribAccessDict(json_object)
+        return json_object
+
+
+    @staticmethod
+    def check_keyword(self, text, keyword):
+
+        reply = False
+
+        content = self.cleanhtml(self, text)
+        content = self.unescape(self, content)
+
+        try:
+            start = content.index("@")
+            end = content.index(" ")
+            if len(content) > end:
+                content = content[0: start:] + content[end +1::]
+
+            cleanit = content.count('@')
+
+            i = 0
+            while i < cleanit :
+                start = content.rfind("@")
+                end = len(content)
+                content = content[0: start:] + content[end +1::]
+                i += 1
+
+            content = content.lower()
+            question = content
+
+            keyword_length = len(keyword)
+
+            if unidecode.unidecode(question)[0:keyword_length] == keyword:
+                reply = True
+
+        except:
+            pass
+
+        return (reply, question)
+
+
     @staticmethod
     def cleanhtml(self, raw_html):
 
@@ -220,11 +173,25 @@ class Mastobot:
         cleantext = re.sub(cleanr, '', raw_html)
         return cleantext
 
+
     @staticmethod
     def unescape(self, s):
 
         s = s.replace("&apos;", "'")
         return s
+
+
+    def replay(self, mention, aux_text):
+
+        post_text  = f"@{mention.username}{aux_text}"
+
+        post_text = (post_text[:400] + '... ') if len(post_text) > 400 else post_text
+
+        self.mastodon.status_post(post_text, in_reply_to_id=mention.status_id,visibility=mention.visibility)
+
+        # print(f'Replied notification {mention.id}')
+        self.mastodon.notifications_dismiss(mention.id)
+
 
     @staticmethod
     def modify_file(self, file_name, pattern,value=""):
@@ -238,6 +205,7 @@ class Mastobot:
             sys.stdout.write(line)
 
         fh.close()
+
 
     def setup(self):
 
@@ -360,4 +328,3 @@ class Mastobot:
             sys.stdout.write(f'\n{str(a_error)}\n')
 
         return (logged_in, mastodon, self.mastodon_hostname)
-

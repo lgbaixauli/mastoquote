73cb73fb73b584f1acb03e945c0a32ff5b3bf644
lgbaixauli <lgbaixauli@gmail.com>
2022-12-13 23:44:09 +0100

    [202212132344]

--- quote.py
+++ quote.py
@@ -17,26 +17,11 @@ class Bot(Mastobot):
 
     def __init__(self, botname: str = BOT_NAME) -> None:
 
-        self._translator = Translator("es")
-
         super().__init__(botname = botname)
 
+        self.init_replay_bot()
+        self.init_translator()
 
-    def init_test_options (self):
-
-        self._dismiss_disable = self._config.get("testing.disable_dismiss")
-        self._test_word       = self._config.get("testing.text_word")
-
-        self._logger.debug ("test_word: "   + self._test_word)
-        self._logger.debug ("ignore test: " + str(self._config.get("testing.ignore_test_toot")))
-
-        if self._test_word != "" and self._config.get("testing.ignore_test_toot"):
-            self._ignore_test = True
-        else:
-            self._ignore_test = False 
-
-        super().init_test_options()
- 
 
     def run(self, botname: str = BOT_NAME) -> None:
 
@@ -44,41 +29,31 @@ class Bot(Mastobot):
 
         for notif in notifications:
 
-            dismiss = True
+            action   = self._actions["Quote_HAL"]   
+            replay, dismiss1 = self.process_notif(notif, "mention", action["keyword"])
+            if replay:
+                self.replay_toot(self.find_text(notif, action), notif)
+     
+            action   = self._actions["Quote_Terry"]
+            replay, dismiss2 = self.process_notif(notif, "mention", action["keyword"])
+            if replay: 
+                self.replay_toot(self.find_text(notif, action), notif)
 
-            if notif.type == 'mention':
-
-                if self._dismiss_disable:
-                    dismiss = False
-                    self._logger.debug("dismissing disabled notification id " + str(notif.id))                    
-    
-                if self._ignore_test and self.check_keyword_in_nofit(notif, self._test_word):
-                    dismiss = False
-                    self._logger.info("ignoring test notification id " + str(notif.id))
-                else: 
-
-                    for key in self._actions:
-
-                        action = self._actions[key]
-
-                        if self.check_keyword_in_nofit(notif, action["keyword"]):
-                            text_post = self.find_text(notif.status.language, action["name"], action["keyword"], action["quotes"])
-
-                            if self._push_disable:
-                                self._logger.info("pushing answer disabled notification id " + str(notif.id))                     
-                            else:
-                                self._logger.info("answering notification id " + str(notif.id))
-                                self.replay(notif, text_post)
-
-            if dismiss:
-                self._logger.debug("dismissing notification id " + str(notif.id))
+            if dismiss1 and dismiss2:
                 self.mastodon.notifications_dismiss(notif.id)
 
         super().run(botname = botname)
 
 
-    def find_text(self, language, name, keyword, quotes):        
+    def find_text(self, notif, action):        
 
+        language = notif.status.language
+        username = notif.account.acct
+        name     = action["name"]
+        keyword  = action["keyword"]
+        quotes   = action["quotes"]
+        quote    = random.choice(quotes)
+ 
         self._logger.debug("notif language: " + language)                    
         self._logger.debug("notif name    : " + name)                    
         self._logger.debug("notif keyword : " + keyword)                    
@@ -86,10 +61,9 @@ class Bot(Mastobot):
         self._translator.fix_language (language)
         _text     = self._translator.get_text
     
-        quote = random.choice(quotes)
         self._logger.debug ("quote id: " + str(quote["id"]))
 
-        post_text  = ", " +_text("cita") + ": \n\n" + quote["text"] + "\n"
+        post_text  = "@" + username + ", " +_text("cita") + ": \n\n" + quote["text"] + "\n"
 
         if "comments" in quote:
             if quote["comments"] != "":

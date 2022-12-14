f89848fbaf0658b3d1e6d51ad3df5298768067b7
lgbaixauli <lgbaixauli@gmail.com>
2022-12-14 20:39:21 +0100

    [202212142039]

--- settst.sh
+++ settst.sh
@@ -5,4 +5,6 @@ cd /home/mastoquote/mastoquote/
 sed -i 's/ignore_test_toot: True/ignore_test_toot: False/g' config.yaml
 sed -i 's/disable_post: False/disable_post: True/g' config.yaml
 sed -i 's/disable_dismiss: False/disable_dismiss: True/g' config.yaml
-sed -i 's/loglevel: 20/loglevel: 10' config.yaml
+sed -i 's/loglevel: 20/loglevel: 10/g' config.yaml
+          
+
